cd1302acf1721ce3256f85a4ff0dbef1862f877e
Lahiry <raphaellcmb@al.insper.edu.br>
2020-09-19 15:55:17 -0300

    wip

--- Coding Dojo/coding_dojo.py
+++ Coding Dojo/coding_dojo.py
@@ -13,28 +13,29 @@ from random import *
 nome = input('Qual o seu nome grande heroi(a)? ')
 
 #Pontos de vida
-HP = 100
+HP = 200
 
 #Pontos de magia
-mana = 100
+mana = 50
 
-#Catalogo de pontos
-pontos_machado = [i for i in range(0,15,1)]
-pontos_espada = [i for i in range(15,25,1)]
-pontos_adaga = [i for i in range(4,45,1)]
+#Catalogo de dano
+dano_machado = randint(10, 20)         #[i for i in range(0,15,1)]
+dano_espada = randint(5, 10)           #([i for i in range(15,25,1)]
+dano_adaga = randint(1, 5)              #[i for i in range(4,45,1)]
 
 #Armas
 armas = {
-    'Machado':,
-    'Espada':,
-    ''
+    'Machado': 1,
+    'Espada': 2,
+    'Adaga': 3
 }
 
 #Armadura
 armadura = {
-    'Magica':1000,
-    'Fisica':1000,
+    'Magica':0.05,
+    'Fisica':0.1,
 }
+
 
 #Danos
 
