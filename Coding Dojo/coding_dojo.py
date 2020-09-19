c7c7b17a45f94ea87cdda2b2350b0514f833784d
wilgnerl <wilgnerl@al.insper.edu.br>
2020-09-19 15:44:53 -0300

    Meu turno

---
+++ Coding Dojo/coding_dojo.py
@@ -0,0 +1,41 @@
+# Integrantes
+# Wilgner Lopes
+# Raphael Lahiry
+# Bruno Domingues
+# Pietro
+
+
+# Declarando biblioteca
+from time import *
+from random import *
+
+# Nome do personagem
+nome = input('Qual o seu nome grande heroi(a)? ')
+
+#Pontos de vida
+HP = 100
+
+#Pontos de magia
+mana = 100
+
+#Catalogo de pontos
+pontos_machado = [i for i in range(0,15,1)]
+pontos_espada = [i for i in range(15,25,1)]
+pontos_adaga = [i for i in range(4,45,1)]
+
+#Armas
+armas = {
+    'Machado':,
+    'Espada':,
+    ''
+}
+
+#Armadura
+armadura = {
+    'Magica':1000,
+    'Fisica':1000,
+}
+
+#Danos
+
+#
