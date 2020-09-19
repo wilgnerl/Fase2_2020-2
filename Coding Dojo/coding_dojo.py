5af5cf97d67408bede233e4cb83de0acde805b52
BrunoSDomingues <brunao.signorelli@hotmail.com>
2020-09-19 16:05:22 -0300

    Added functions

--- Coding Dojo/coding_dojo.py
+++ Coding Dojo/coding_dojo.py
@@ -6,7 +6,6 @@
 
 
 # Declarando biblioteca
-from time import *
 from random import *
 
 # Nome do personagem
@@ -19,9 +18,13 @@ HP = 200
 mana = 50
 
 #Catalogo de dano
-dano_machado = randint(10, 20)         #[i for i in range(0,15,1)]
-dano_espada = randint(5, 10)           #([i for i in range(15,25,1)]
-dano_adaga = randint(1, 5)              #[i for i in range(4,45,1)]
+def calc_dmg(id_weapon):
+    if id_weapon == 1: # Machado
+        return randint(10, 20)         #[i for i in range(0,15,1)]
+    elif id_weapon == 2: # Espada
+        return randint(5, 10)           #([i for i in range(15,25,1)]
+    elif id_weapon == 3: # Adaga
+        return randint(1, 5)              #[i for i in range(4,45,1)]
 
 #Armas
 armas = {
@@ -37,6 +40,20 @@ armadura = {
 }
 
 
-#Danos
+# Magia
+# Caso 1: self damage - 10%
+# Caso 2: nada acontece - 20%
+# Caso 3: dano normal - 50%
+# Caso 4: dano critico - 20%
 
-def dano_magico(magia)
+# 5 mana pra usar
+def use_magic(mana):
+    if mana < 5:
+        return "Sem mana para uso de magia!"
+    else:
+        mana -= 5
+        casos_magia = [1, 2, 2, 3, 3, 3, 3, 3, 4, 4]
+        out = choice(casos_magia)
+
+        return mana, out
+
